09ee427125b1e358836bd6ef417974a033752cd6
Carla <carlitaormina93@gmail.com>
2025-05-08 22:12:55 -0300

    modulos

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { EmotionsModule } from './emotions/emotions.module';
+import { NotificationsModule } from './notifications/notifications.module';
+import { ResourcesModule } from './resources/resources.module';
+import { UsersModule } from './users/users.module';
+import { FileUploadModule } from './file-upload/file-upload.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, EmotionsModule, NotificationsModule, ResourcesModule, UsersModule, FileUploadModule],
   controllers: [AppController],
   providers: [AppService],
 })
