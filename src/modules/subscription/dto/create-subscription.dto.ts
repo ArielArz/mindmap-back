13a1198174391fef2db22dd4176b8f4847362416
MiguelFuenzalida <miguelfuenzalida.n@gmail.com>
2025-05-25 21:52:03 -0400

    isTrial Fixed

--- src/modules/subscription/dto/create-subscription.dto.ts
+++ src/modules/subscription/dto/create-subscription.dto.ts
@@ -1,5 +1,5 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { IsString, IsUUID } from 'class-validator';
+import { IsOptional, IsString, IsUUID } from 'class-validator';
 
 export class CreateSubscriptionDto {
   @ApiProperty({
@@ -13,6 +13,7 @@ export class CreateSubscriptionDto {
     description: 'ID de la sesión de Stripe generada en el front',
     example: 'cs_test_a1b2c3d4e5f6g7h8i9j0',
   })
+  @IsOptional()
   @IsString()
   sessionId: string;
 }
