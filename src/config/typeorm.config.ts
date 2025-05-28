e2cb937916bf445993bc5710f77d4050c9e2d94c
Ariel Arzamendia <arielarzamendia@gmail.com>
2025-05-28 15:32:14 -0300

    deploy

--- src/config/typeorm.config.ts
+++ src/config/typeorm.config.ts
@@ -23,6 +23,13 @@
 import { registerAs } from '@nestjs/config';
 import { DataSource, DataSourceOptions } from 'typeorm';
 import { DB_HOST, DB_NAME, DB_PASSWORD, DB_PORT, DB_USER } from './env';
+import { Contact } from 'src/modules/contact/entities/contact.entity';
+import { Emotion } from 'src/modules/emotions/entities/emotion.entity';
+import { HelpPoint } from 'src/modules/helppoints/entities/helppoint.entity';
+import { Resource } from 'src/modules/resources/entities/resource.entity';
+import { Subscription } from 'src/modules/subscription/entities/subscription.entity';
+import { User } from 'src/modules/users/entities/user.entity';
+import { UserState } from 'src/modules/user-state/entities/user-state.entity';
 
 const isProd = process.env.NODE_ENV === 'developer';
 
@@ -32,15 +39,15 @@ const config: DataSourceOptions = {
   ssl: {
     rejectUnauthorized: false,
   },
-  host: DB_HOST,
-  port: DB_PORT,
-  username: DB_USER,
-  password: DB_PASSWORD,
-  database: DB_NAME,
-  entities: ['dist/**/*.entity{.ts,.js}'],
+  // host: DB_HOST,
+  // port: DB_PORT,
+  // username: DB_USER,
+  // password: DB_PASSWORD,
+  // database: DB_NAME,
+  entities: [Contact, Emotion, HelpPoint, Resource, Subscription, User, UserState],
   migrations: [],
   subscribers: [],
-  synchronize: true,
+  synchronize: false,
   dropSchema: false,
   logging: !isProd,
 };
