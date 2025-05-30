8c9531495896b08fc0708c1911a9d1035b44691a
MiguelFuenzalida <miguelfuenzalida.n@gmail.com>
2025-05-30 18:34:37 -0400

    Agrega busqueda a emociones por nombre o valor clínico

--- src/modules/users/dto/change-role.dto.ts
+++ src/modules/users/dto/change-role.dto.ts
@@ -1,10 +1,20 @@
 import { IsEnum, IsUUID } from 'class-validator';
-import { UserRole } from '../entities/enum/user-role.enum'; // Asegúrate de que esta ruta sea correcta
+import { ApiProperty } from '@nestjs/swagger';
+import { UserRole } from '../entities/enum/user-role.enum';
 
 export class ChangeRoleDto {
+  @ApiProperty({
+    description: 'ID del usuario al que se le cambiará el rol',
+    example: 'd9b1c2f6-4cfa-4d9f-9a15-8df5d0f1f9e2',
+  })
   @IsUUID()
   userId: string;
 
+  @ApiProperty({
+    description: 'Nuevo rol del usuario',
+    enum: UserRole,
+    example: UserRole.PREMIUM,
+  })
   @IsEnum(UserRole, { message: 'El rol debe ser válido' })
   newRole: UserRole;
 }
