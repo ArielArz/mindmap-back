5af8d7d5c57498d3b51dd45771f71180d333f8cc
Ariel Arzamendia <arielarzamendia@gmail.com>
2025-06-04 17:04:16 -0300

    borrando/comentarios

--- src/modules/contact/contact.service.ts
+++ src/modules/contact/contact.service.ts
@@ -1,6 +1,5 @@
 import { ConflictException, Injectable } from '@nestjs/common';
 import { CreateContactDto } from './dto/create-contact.dto';
-import { UpdateContactDto } from './dto/update-contact.dto';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Contact } from './entities/contact.entity';
 import { Repository } from 'typeorm';
@@ -24,21 +23,5 @@ export class ContactService {
     await this.contactRepo.save(message);
 
     return { message: 'Mensaje enviado correctamente' };
-  }
-
-  findAll() {
-    return this.contactRepo.find();
-  }
-
-  findOne(id: number) {
-    return `This action returns a #${id} contact`;
-  }
-
-  update(id: number, updateContactDto: UpdateContactDto) {
-    return `This action updates a #${id} contact`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} contact`;
   }
 }
